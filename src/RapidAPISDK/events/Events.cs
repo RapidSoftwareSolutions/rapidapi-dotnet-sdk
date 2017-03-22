a84ab4ccbe53ed96ab4aa760af4ae73c678175d2
miki.lior <miki.lior@rapidapi.com>
2017-03-22 13:56:16 +0200

    Add events for callback support

---
+++ src/RapidAPISDK/events/Events.cs
@@ -0,0 +1,8 @@
+﻿using System;
+namespace RapidAPISDK.Events
+{
+	public delegate void MessageCallback(string message);
+	public delegate void ErrorCallback(string reason);
+	public delegate void TimeOutCallback();
+	public delegate void ConnectCallback();
+}
